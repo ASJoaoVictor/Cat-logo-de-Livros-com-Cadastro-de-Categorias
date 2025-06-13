7a19bedbdf14f9aaa8c105e9ad908d1f0840a625
ASJoaoVictor <victor.sidiao@academico.ifpb.edu.br>
2025-06-13 01:08:28 +0000

    listem de categorias

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -6,22 +6,25 @@ const categorias = [
 
 const buttonCadastrarLivro = document.getElementById("button-cadastrar-livro");
 const buttonCadastrarCategoria = document.getElementById("button-cadastrar-categoria");
+const buttonListarCategorias = document.getElementById("button-listar-categorias")
 
 
 const formCategoria = document.getElementById("form-categoria");
+const listaCategorias = document.getElementById("lista-categorias");
 
 formCategoria.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const nomeCategoria = document.getElementById("nome-categoria").value.toLowerCase(); 
 
-    alert(nomeCategoria);
     if(categorias.find(c => c === nomeCategoria)){
         alert("Categoria já existe");
     }else{
         categorias.push(nomeCategoria);
         console.log(categorias);
     }
+
+    formCategoria.reset();
 });
 
 buttonCadastrarLivro.addEventListener("click", (e) => {
@@ -36,4 +39,13 @@ buttonCadastrarCategoria.addEventListener("click", (e) => {
 
     isButtonHigh = buttonCadastrarCategoria.classList == "ativo"
     buttonCadastrarCategoria.classList = isButtonHigh ? "desativo" : "ativo";
+});
+
+buttonListarCategorias.addEventListener("click", () => {
+    listaCategorias.innerHTML = "";
+    for (let index = 0; index < categorias.length; index++) {
+        listaCategorias.innerHTML += "<p>" + categorias[index] + "</p>"
+        
+    }
+
 });
