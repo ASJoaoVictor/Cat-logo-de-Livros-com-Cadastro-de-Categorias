0af56ec66ddf6fa6486acc2006ab6ca15f8559d8
ASJoaoVictor <victor.sidiao@academico.ifpb.edu.br>
2025-06-14 22:14:16 +0000

    js do menu

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -7,14 +7,14 @@ const categorias = [
 const buttonCadastrarLivro = document.getElementById("button-cadastrar-livro");
 const buttonCadastrarCategoria = document.getElementById("button-cadastrar-categoria");
 const buttonListarCategorias = document.getElementById("button-listar-categorias")
+const buttonListarLivros = document.getElementById("button-listar-livros")
 
 const formLivro = document.getElementById("form-livro");
 const formCategoria = document.getElementById("form-categoria");
 const listaCategorias = document.getElementById("lista-categorias");
+const listaLivros = document.getElementById("lista-livros");
 
 const selectCategoriaLivro = document.getElementById("select-categoria-livro");
-
-const divFormLivro = document.getElementById("div-form-livro");
 
 formCategoria.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -31,15 +31,20 @@ formCategoria.addEventListener("submit", (e) => {
     formCategoria.reset();
 });
 
+
+//Funções dos botões do menu
 buttonCadastrarLivro.addEventListener("click", (e) => {
     e.preventDefault();
 
-    isButtonHigh = buttonCadastrarLivro.classList == "ativo"
     buttonCadastrarLivro.classList = "ativo";
     buttonCadastrarCategoria.classList =  "desativo";
+    buttonListarCategorias.classList = "desativo";
+    buttonListarLivros.classList = "desativo";
 
-    formLivro.classList = "";
-    formCategoria.classList = "hidden";
+    formLivro.style.display = "";
+    formCategoria.style.display = "none";
+    listaCategorias.style.display = "none";
+    listaLivros.style.display = "none";
 
     selectCategoriaLivro.innerHTML = ""
     for (let index = 0; index < categorias.length; index++) {
@@ -52,21 +57,50 @@ buttonCadastrarLivro.addEventListener("click", (e) => {
 buttonCadastrarCategoria.addEventListener("click", (e) => {
     e.preventDefault();
 
-    isButtonHigh = buttonCadastrarCategoria.classList == "ativo"
-    buttonCadastrarCategoria.classList = "ativo";
+    buttonCadastrarCategoria.classList =  "ativo";
     buttonCadastrarLivro.classList = "desativo";
+    buttonListarCategorias.classList = "desativo";
+    buttonListarLivros.classList = "desativo";
 
-    formCategoria.classList = "";
-    formLivro.classList = "hidden";
+    formCategoria.style.display = "";
+    listaCategorias.style.display = "none";
+    formLivro.style.display = "none";
+    listaLivros.style.display = "none";
 });
 
 buttonListarCategorias.addEventListener("click", () => {
+
+    buttonListarCategorias.classList = "ativo";
+    buttonCadastrarLivro.classList = "desativo";
+    buttonCadastrarCategoria.classList =  "desativo";
+    buttonListarLivros.classList = "desativo";
+
+    listaCategorias.style.display = "";
+    formLivro.style.display = "none";
+    formCategoria.style.display = "none";
+    listaLivros.style.display = "none";
+
     listaCategorias.innerHTML = "";
+    listaCategorias.innerHTML += "<h2>Lista de Categorias</h2>"
     for (let index = 0; index < categorias.length; index++) {
         listaCategorias.innerHTML += "<p>" + categorias[index] + "</p>"
         
     }
 
+});
+
+buttonListarLivros.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    buttonListarLivros.classList = "ativo";
+    buttonListarCategorias.classList = "desativo";
+    buttonCadastrarLivro.classList = "desativo";
+    buttonCadastrarCategoria.classList =  "desativo";
+
+    listaLivros.style.display = "";
+    listaCategorias.style.display = "none";
+    formLivro.style.display = "none";
+    formCategoria.style.display = "none";
 });
 
 buttonCadastrarLivro.click();
